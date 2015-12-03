75504b5b4d12c50fa9a0045bd12bac44bde142c1
Michael <rosner.17@gmail.com>
2015-12-03 20:16:01 +0000

    minor changes

--- 15_12_led_test.py
+++ 15_12_led_test.py
@@ -6,7 +6,7 @@ GPIO.setup(7, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
 try:
     while True:
       GPIO.output(7,True) ## Turn on GPIO pin 7
-    except KeyboardInterrupt:
-      GPIO.cleanup()
+except KeyboardInterrupt:
+    GPIO.cleanup()
 
 
