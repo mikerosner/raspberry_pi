5da3f23559aeff98366e4988b73f1bc6ba3f854d
mikerosner <mikerosner@users.noreply.github.com>
2015-12-03 14:57:24 -0500

    Update 15_12_led_test.py

--- 15_12_led_test.py
+++ 15_12_led_test.py
@@ -1,12 +1,12 @@
 import RPi.GPIO as GPIO ## Import GPIO library
-from msvcrt import getch
 
 GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
 GPIO.setup(7, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
 
-while true : 
-  key = ord(getch())
-  GPIO.output(7,True) ## Turn on GPIO pin 7
-  if key == 13: #Enter
-    break
-GPIO.cleanup()
+try:
+    while True:
+      GPIO.output(7,True) ## Turn on GPIO pin 7
+    except KeyboardInterrupt:
+      GPIO.cleanup()
+
+
