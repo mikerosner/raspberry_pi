6062f006adedde8ebc67e24e8c29ca6144c61ab6
mikerosner <mikerosner@users.noreply.github.com>
2015-12-04 19:43:45 -0500

    Update 15_12_reaction_game.py

--- 15_12_reaction_game.py
+++ 15_12_reaction_game.py
@@ -2,17 +2,51 @@ import RPi.GPIO as GPIO ## Import GPIO library
 import time
 
 GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
-GPIO.setup(7, GPIO.in) ## Setup GPIO Pin 7 to IN
+GPIO.setup(7, GPIO.IN) ## Setup GPIO Pin 7 to IN
 GPIO.setup(11, GPIO.IN) ## Setup GPIO Pin 11 to IN
+winner = 0
 
+## Sample light turn on code - GPIO.output(7,True) ## Turn on GPIO pin 7
+## TBD turn on light for random 5-10 seconds
+## Turn off light
+
+if GPIO.input(7) :
+    control1 = 1
+else :
+    control1 = 0
+    
+if GPIO.input(11) :
+    control2 = 1
+else :
+    control2 = 0
+    
 try:
     while True:
-      GPIO.output(7,True) ## Turn on GPIO pin 7
-      time.sleep(1) ## wait 1 second
-      reading = GPIO.input(11) ## read pin 11
-      if reading :
-        print "on"
-      else : 
-        print "off"
+        btn1 = GPIO.input(7)
+        btn2 = GPIO.input(11)
+        if control1 = 0 :
+            if btn1 = 1 :
+                winner = 1
+                break
+        else :
+            if btn1 = 0 :
+                control2 = 0 ## if button 1 is not pressed, assign control1 = 0
+        
+        if control2 = 0 :
+            if btn2 = 1 :
+                winner = 2
+                break
+        else :
+            if btn2 = 0 :
+                control2 = 0 ## if button 2 is not pressed, assign control2 = 0
+        
+        if reading :
+            print "on"
+        else : 
+            print "off"
 except KeyboardInterrupt:
     GPIO.cleanup()
+
+print winner
+sleep(5)
+GPIO.cleanup()
