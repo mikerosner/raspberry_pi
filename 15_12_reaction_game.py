68c9ec53e89a92a9a449fe06f86e0a57c66d2dcf
mikerosner <mikerosner@users.noreply.github.com>
2015-12-04 22:28:37 -0500

    Update 15_12_reaction_game.py

--- 15_12_reaction_game.py
+++ 15_12_reaction_game.py
@@ -1,29 +1,44 @@
 import RPi.GPIO as GPIO ## Import GPIO library
 import time
+import random
+
+red_led = 0
+ylw_led = 0
+grn_led = 0
+ply_1 = 7
+ply_2 = 11
 
 GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
-GPIO.setup(7, GPIO.IN) ## Setup GPIO Pin 7 to IN
-GPIO.setup(11, GPIO.IN) ## Setup GPIO Pin 11 to IN
+GPIO.setup(ply_1, GPIO.IN) ## Setup GPIO Pin 7 to IN
+GPIO.setup(ply_2, GPIO.IN) ## Setup GPIO Pin 11 to IN
 winner = 0
 
-## Sample light turn on code - GPIO.output(7,True) ## Turn on GPIO pin 7
-## TBD turn on light for random 5-10 seconds
-## Turn off light
+print "Get Ready..."
+GPIO.output(red_led,True)
+time.sleep(2)
+print "Get Set..."
+GPIO.output(red_led,False)
+GPIO.output(ylw_led,True)
+time.sleep(random.uniform(3,5))
+print "GO!"
+print
+GPIO.output(ylw_led,False)
+GPIO.output(grn_led,True)
 
-if GPIO.input(7) :
+if GPIO.input(ply_1) :
     control1 = 0
 else :
     control1 = 1
-    
-if GPIO.input(11) :
+
+if GPIO.input(ply_2) :
     control2 = 0
 else :
     control2 = 1
-    
+
 try:
     while True:
-        btn1 = GPIO.input(7)
-        btn2 = GPIO.input(11)
+        btn1 = GPIO.input(ply_1)
+        btn2 = GPIO.input(ply_2)
         if control1 == 0 :
             if btn1 == 0 :
                 winner = 1
@@ -31,7 +46,7 @@ try:
         else :
             if btn1 == 1 :
                 control1 = 0 ## if button 1 is not pressed, assign control1 = 0
-        
+
         if control2 == 0 :
             if btn2 == 0 :
                 winner = 2
@@ -43,6 +58,7 @@ try:
 except KeyboardInterrupt:
     GPIO.cleanup()
 
-print winner
+GPIO.output(grn_led,False)
+print "Player # " + winner + " is the winner!"
 time.sleep(5)
 GPIO.cleanup()
