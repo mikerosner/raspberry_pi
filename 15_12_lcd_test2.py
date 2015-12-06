0a98842324820c0e15e818ba668b0e8f88e263a9
mikerosner <mikerosner@users.noreply.github.com>
2015-12-06 14:58:38 -0500

    Update 15_12_lcd_test2.py

--- 15_12_lcd_test2.py
+++ 15_12_lcd_test2.py
@@ -1,1 +1,12 @@
+import RPi.GPIO as GPIO ## Import GPIO library
+import time
+import random
+import lcd_class
 
+lcd = lcd_class.lcd_data()
+
+lcd.init()
+temp_str = "A"
+lcd.write_char(temp_str)
+
+time.sleep(2)
