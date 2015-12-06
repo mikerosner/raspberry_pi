ce9818a5e1c37c75dd9b91bdc8b98269f1799df0
mikerosner <mikerosner@users.noreply.github.com>
2015-12-06 10:16:24 -0500

    Update 15_12_lcd_class.py

--- 15_12_lcd_class.py
+++ 15_12_lcd_class.py
@@ -3,16 +3,16 @@ import ctypes
 c_uint8 = ctypes.c_uint8
 class lcd_data_bits( ctypes.LittleEndianStructure ):
   _fields_ = [
-    ("db0"
-    ("rw",  c_uint8, 1 ),  # asByte & 2
-    ("db7", c_uint8, 1 ),  # asByte & 4
-    ("db6", c_uint8, 1 ),  # asByte & 8
-    ("db5", c_uint8, 1 ),  # asByte & 16
-    ("db4", c_uint8, 1 ),  # asByte & 32
-    ("db3", c_uint8, 1 ),  # asByte & 64
-    ("db2", c_uint8, 1 ),  # asByte & 128
-    ("db1", c_uint8, 1 ),  # asByte & 256
     ("db0", c_uint8, 1 ),  # asByte & 512
+    ("db1", c_uint8, 1 ),  # asByte & 256
+    ("db2", c_uint8, 1 ),  # asByte & 128
+    ("db3", c_uint8, 1 ),  # asByte & 64
+    ("db4", c_uint8, 1 ),  # asByte & 32
+    ("db5", c_uint8, 1 ),  # asByte & 16
+    ("db6", c_uint8, 1 ),  # asByte & 8
+    ("db7", c_uint8, 1 ),  # asByte & 4
+    ("rw",  c_uint8, 1 ),  # asByte & 2
+    ("rs",  c_uint8, 1 ),  # asByte & 1
   ]
   
 class lcd_data( ctypes.Union ):
